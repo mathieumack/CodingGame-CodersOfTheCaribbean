05f2d5318081c253e17f8c61dc93551e1c3fc2f8
Nicolas BARBE <nicolas.barbe@econocom.com>
2017-04-19 20:04:06 +0200

    nicocode

--- Program.cs
+++ Program.cs
@@ -1,43 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CodingGameCodeSample
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+
+        // game loop
+        while (true)
         {
-            int cols = 22;
-            int rows = 21;
+            int barrelX = 0;
+            int barrelY = 0;
 
-            // game loop
-            while (true)
+            int myShipCount = int.Parse(Console.ReadLine()); // the number of remaining ships
+            int entityCount = int.Parse(Console.ReadLine()); // the number of entities (e.g. ships, mines or cannonballs)
+            for (int i = 0; i < entityCount; i++)
             {
-                int myShipCount = int.Parse(Console.ReadLine()); // the number of remaining ships
-                int entityCount = int.Parse(Console.ReadLine()); // the number of entities (e.g. ships, mines or cannonballs)
-                for (int i = 0; i < entityCount; i++)
-                {
-                    string[] inputs = Console.ReadLine().Split(' ');
-                    int entityId = int.Parse(inputs[0]);
-                    string entityType = inputs[1];
-                    int x = int.Parse(inputs[2]);
-                    int y = int.Parse(inputs[3]);
-                    int arg1 = int.Parse(inputs[4]);
-                    int arg2 = int.Parse(inputs[5]);
-                    int arg3 = int.Parse(inputs[6]);
-                    int arg4 = int.Parse(inputs[7]);
-                }
-                for (int i = 0; i < myShipCount; i++)
-                {
-                    var random = new Random();
-                    // Write an action using Console.WriteLine()
-                    // To debug: Console.Error.WriteLine("Debug messages...");
+                string[] inputs = Console.ReadLine().Split(' ');
+                int entityId = int.Parse(inputs[0]);
+                string entityType = inputs[1];
 
-                    Console.WriteLine("MOVE " + ((int)random.Next(0, cols + 1)).ToString() + " " + ((int)random.Next(0, rows + 1)).ToString() + ""); // Any valid action, such as "WAIT" or "MOVE x y"
+                int x = int.Parse(inputs[2]);
+                int y = int.Parse(inputs[3]);
+
+                // get one barrel 
+                if (entityType == "BARREL")
+                {
+                    barrelX = x;
+                    barrelY = y;
                 }
+
+                int arg1 = int.Parse(inputs[4]);
+                int arg2 = int.Parse(inputs[5]);
+                int arg3 = int.Parse(inputs[6]);
+                int arg4 = int.Parse(inputs[7]);
+            }
+            for (int i = 0; i < myShipCount; i++)
+            {
+
+                // Write an action using Console.WriteLine()
+                // To debug: Console.Error.WriteLine("Debug messages...");
+
+                Console.WriteLine("MOVE " + barrelX.ToString() + " " + barrelY.ToString()); // Any valid action, such as "WAIT" or "MOVE x y"
             }
         }
     }
